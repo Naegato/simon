23ed70df40bd2edaac0a9962540e8ebea7701465
Naegato <maxime.wiatr@gmail.com>
2022-11-08 14:54:30 +0100

    fix/init

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express'); //Import the express dependency
+const path = require('path');
 const app = express();              //Instantiate an express app, the main work horse of this server
-const port = 5000;                  //Save the port number where your server will be listening
+const port = 5000;                //Save the port number where your server will be listening
 
+app.use(express.static(path.join(__dirname, 'public')))
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
+    res.sendFile('/', {root: __dirname});      //server responds by sending the index.html file to the client's browser
                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile
 });
 
